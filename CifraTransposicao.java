50b5fa4417dab7242b4b9b54e64b1d570ed13b68
David Freitas <dvdcomputacao@gmail.com>
2022-06-24 20:09:31 -0300

    Cifragem por Transposição

---
+++ CifraTransposicao.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class CifraTransposicao {
+    static Scanner scan = new Scanner(System.in);
+
+    public static int[] ColocarCaractereChaveEmOrdemAlfabetica(String chave) {
+        char[] vcChave = chave.toCharArray();
+        char[] vcChaveOrdenada = chave.toCharArray();
+        int[] indicesDaChaveOrdenados = new int[vcChave.length];
+        Arrays.sort(vcChaveOrdenada);
+
+        for (int j = 0, k = 0; j < vcChave.length; j++) {
+            for (int i = 0; i < vcChave.length; i++) {
+                if (vcChave[i] == vcChaveOrdenada[j]) {
+                    indicesDaChaveOrdenados[k] = i;
+                    k++;
+                }
+            }
+            j = k - 1;
+        }
+        return indicesDaChaveOrdenados;
+    }
+
+    public static char[][] ColocarRegistroNaMatriz(String registro, char[] vcChave) {
+        int numDeLinhas = registro.length() / vcChave.length;
+        if (registro.length() % vcChave.length > 0)
+            numDeLinhas++;
+        int numDeColunas = vcChave.length;
+        int indRegistro = 0;
+        char[][] MatrizComRegistro = new char[numDeLinhas][vcChave.length];
+        char[] vcRegistro = registro.toCharArray();
+
+        for (int i = 0; i < numDeLinhas && indRegistro < registro.length(); i++) {// linha
+            for (int j = 0; j < numDeColunas && indRegistro < registro.length(); j++) {// coluna
+                MatrizComRegistro[i][j] = vcRegistro[indRegistro++];
+            }
+        }
+        return MatrizComRegistro;
+    }
+
+    public static char[][] CriptografarRegistro(char[][] matrizComRegistro, String chave) {
+        int[] viPosicaoOrdenada = ColocarCaractereChaveEmOrdemAlfabetica(chave);
+        char[][] matriz = new char[matrizComRegistro.length][viPosicaoOrdenada.length];
+
+        for (int j = 0; j < viPosicaoOrdenada.length; j++) { // coluna
+            for (int i = 0; i < matrizComRegistro.length; i++) {// linha
+                matriz[i][j] = matrizComRegistro[i][viPosicaoOrdenada[j]];
+            }
+        }
+        return matriz;
+    }
+
+    public static char[][] DescriptografarRegistro(char[][] matrizCriptografada, String chave) {
+        int[] viPosicaoOrdenada = ColocarCaractereChaveEmOrdemAlfabetica(chave);
+        char[][] matrizDescriptografada = new char[matrizCriptografada.length][viPosicaoOrdenada.length];
+
+        for (int j = 0; j < viPosicaoOrdenada.length; j++) { // coluna
+            for (int i = 0; i < matrizCriptografada.length; i++) {// linha
+                matrizDescriptografada[i][viPosicaoOrdenada[j]] = matrizCriptografada[i][j];
+            }
+        }
+        return matrizDescriptografada;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Entre com a chave");
+        String chave = scan.nextLine();
+        System.out.println("Entre com o registro");
+        String registro = scan.nextLine();
+
+        char[] vcChave = chave.toCharArray();
+        char[][] matrizComRegistro = ColocarRegistroNaMatriz(registro, vcChave);
+        char[][] matrizCriptografada = CriptografarRegistro(matrizComRegistro, chave);
+        char[][] registroDescriptografado = DescriptografarRegistro(matrizCriptografada, chave);
+
+    }
+}
