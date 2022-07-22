8632dde754d65b900ef754a6795f91029c97b4c1
David Freitas <dvdcomputacao@gmail.com>
2022-07-22 09:51:35 -0300

    atribui registro à matriz e atualizei o readme

--- CifraTransposicao.c
+++ CifraTransposicao.c
@@ -6,21 +6,34 @@
 #define true 1
 #define false 0
 
-char **ColocarRegistroNaMatriz(char registro[100], char chave[15]){
-    return NULL;
+void criptografarMatriz(char matriz[100][100], char *chave){
+    
 }
 
-int main() 
-{
-    char chave[15] = "";
-    char registro[100] = "";
-    printf("Entre com a chave: ");
-    scanf("%s", chave);
-    printf("Entre com a registro: ");
-    scanf("%s", registro);
+void ColocarRegistroNaMatriz(char *registro, char *chave){
+    int tamanhoRegistro = (int)strlen(registro);
+    int tamanhoChaveNumColunas = (int)strlen(chave);
+    double c = (double) tamanhoRegistro / tamanhoChaveNumColunas;
+    char matriz[100][100]; int i = 0;
 
-    int numDeLinhas = ceil(sizeof(registro)/sizeof(chave));    
-    char matrizComRegistro[numDeLinhas][sizeof(chave)];
-    matrizComRegistro = ColocarRegistroNaMatriz(registro, chave);
+    for (int linha = 0; linha < (int)ceil(c); linha++){
+        for (int coluna = 0; i < strlen(registro) && coluna < tamanhoChaveNumColunas; coluna++){
+            matriz[linha][coluna] = registro[i++];
+        }
+    }
+    criptografarMatriz(matriz, chave);
+}
+
+int main()
+{
+    char *chave = (char *)malloc(sizeof(char) * 15); // aloco 15 espaços na memória
+    char *registro = (char *)malloc(sizeof(char) * 100);
+    printf("Entre com a chave: ");
+    fgets(chave, 15, stdin); // lê até a quebra de linha ('\n')
+    printf("Entre com a registro: ");
+    fgets(registro, 100, stdin); 
+    ColocarRegistroNaMatriz(registro, chave);
+
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Criptografia
 ### Cifra das Colunas(ou Transposição)
+[//]: <> (press `Shift + Ctrl + v` to see preview in Vscode)
 ![cifraDasColunas](https://user-images.githubusercontent.com/77236515/176682137-419a855b-5519-4f47-81d2-53f0f81b9e55.jpeg)
+---
+Para compilar o programa em `C`
+~~~C
+gcc CifraTransposicao.c -lm
+./a.out
+~~~
